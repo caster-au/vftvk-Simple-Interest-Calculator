5519fc65d91155f809397d2d4c37911d1ce60674
caster-au <as0944868@gmail.com>
2021-05-22 14:45:33 +0000

    Edited files

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   var principal = document.getElementById("principal").value;
+   //this will help to identify negative or zero numbers and direct you to again principle
+   if(principal<1){
+       alert("Enter a positive number"); 
+         document.getElementById("principal").focus(); 
+   }
+   else{
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var amount = principal*rate*years/100;
+    var year= new Date().getFullYear()+parseInt(years); 
+    var result= document.getElementById("result");
+    document.getElementById("result").innerHTML="If you deposit "+"<span class='highlight'>" +principal+"</span>"+",\<br\>at an interest rate of "+"<span class='highlight'>"+rate+"%"+"</span>"+"\<br\>You will receive an amount of "+"<span class='highlight'>"+amount+"</span>"+",\<br\>in the year "+"<span class='highlight'>"+year+"</span>"+"\<br\>";
+   }
 }
-        
+function updateRate(amount){
+    var rateval= document.getElementById("rate_val");
+    rateval.innerHTML=amount;
+}    
